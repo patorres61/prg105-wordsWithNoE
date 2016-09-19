5ba608762c95afcbb4a81bb3131e18b98cb93260
Phyllis Torres <ptorres@students.mchenry.edu>
2016-09-19 16:11:20 -0500

    Add files via upload

--- wordsNoE.py
+++ wordsNoE.py
@@ -1,8 +1,9 @@
-# This program will read a text file of words and print out any words that have over 20 characters not including white space
+# This program will read a text file of words and print out any words do not contain the letter E
 
 # Define variables
-NoEcount = 1
+noEcount = 0
 wordsWithE = 0
+totalWords = 0
 
 # Define input file
 fin = open('words.txt')
@@ -10,16 +11,29 @@ fin = open('words.txt')
 # Read input file
 fin.readline()
 
-print ('Words Greater Than 20 characters Omitting Blank Spaces\n')
-# Read file, check character count in each line
-for line in fin:
-    word = line.strip()     # line.strip will strip out any blank spaces
-    if 'e' in word:
-        wordsWithE += 1
-    else:
-        print word
-        NoEcount += 1
+print ('Words Containing No Letter E\n')
 
-print ('\nTotal number of words in file: ' + str(wordsWithE+NoEcount))
+# Read file, check character count in each line
+def has_no_e(word):
+    if 'e' in word:
+        return False
+    else:
+        return True
+
+for line in fin:
+    word = line.strip()  # line.strip will strip out any blank spaces
+    totalWords += 1
+    has_no_e(word)
+    if has_no_e(word) is True:
+        print word
+        noEcount += 1
+    else:
+        wordsWithE += 1
+
+percent = 100 * (noEcount / float(totalWords))
+print noEcount
+print wordsWithE
 print ('\nTotal number of words in file containing the letter E: ' + str(wordsWithE))
-print ('\nTotal number of words in file with no letter E: ' + str(NoEcount))
+print ('\nTotal number of words in file with no letter E: ' + str(noEcount))
+print ('\nTotal number of words in file: ' + str(wordsWithE + noEcount))
+print ('\nPercentage of words in file with no letter E is: ' + str(round(percent, 2)) + '%')
